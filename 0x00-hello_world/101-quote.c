1abbbe002b71edc6da465b03d80f4097d639b121
Anastasiauzor <stasiaof2003@gmail.com>
2023-03-16 16:01:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 
 /**
  * main - this is the main function
- * Return: one when successful
+ * Return: 1 when successful
  */
-
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
