9be615d4ce8f14f1b1843803bd3602711ce17213
Anastasiauzor <stasiaof2003@gmail.com>
2023-03-22 08:11:14 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function
+ *
+ * @a: parameters a
+ *
+ * @b: parameters b
+ *
+ * Return: 0 when successful
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+	_putchar('\n');
+}
