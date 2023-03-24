45fe79c894c0078f9cb56b4ca797a84583f63222
Anastasiauzor <stasiaof2003@gmail.com>
2023-03-24 10:44:13 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - maain function
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	if (i == 0 && i <= 9 && i != 2 && i != 4)
+		_putchar(i);
+	_putchar('\n');
+}
