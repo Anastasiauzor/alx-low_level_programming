3dd5fac05faed8a924c815e8b5f3eca7b5a5fe50
Anastasiauzor <stasiaof2003@gmail.com>
2023-03-27 08:33:01 -0700

    strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function
+ * @str: parameter for strings
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+		_putchar('\n');
+	}
+}
+
